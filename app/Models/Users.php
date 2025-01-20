8bf07422396414914bb2876c6e23ad6a00f3e319
sonypiay <pyscho30@gmail.com>
2025-01-21 01:08:17 +0700

    change relation userLevel to historyScore

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Users extends Model
 {
@@ -17,8 +17,8 @@ class Users extends Model
     protected $keyType = "string";
     public $incrementing = false;
 
-    public function userLevel(): HasOne
+    public function historyScore(): HasMany
     {
-        return $this->hasOne(UserLevel::class, 'user_id', 'id')->withDefault();
+        return $this->hasMany(HistoryScore::class, 'user_id');
     }
 }
