a05ba4b64d83c7e0da6d05089f91205275136f1a
sonypiay <pyscho30@gmail.com>
2025-01-21 01:07:55 +0700

    adjust seeder users

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Users;
-use App\Models\UserLevel;
 
 class UsersSeeder extends Seeder
 {
@@ -14,6 +13,6 @@ class UsersSeeder extends Seeder
      */
     public function run(): void
     {
-        Users::factory(10000)->create();
+        Users::factory(1000)->create();
     }
 }
