6b6a0d288eadbfbcd8e89448209c03000d9e6e80
sonypiay <pyscho30@gmail.com>
2025-01-20 22:40:54 +0700

    create migrations: - table users - table user_level

---
+++ database/migrations/2025_01_20_221217_create_tbl_user_level.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tbl_user_level', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+            ->on('tbl_users')
+            ->references('id')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tbl_user_level');
+    }
+};
