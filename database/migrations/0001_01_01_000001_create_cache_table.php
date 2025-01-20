1b55e0f45eae992a8c8c5d853a928e9cc5d0fd0d
sonypiay <pyscho30@gmail.com>
2025-01-20 22:40:24 +0700

    change table name cache

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cache', function (Blueprint $table) {
+        Schema::create(env('DB_CACHE_TABLE'), function (Blueprint $table) {
             $table->string('key')->primary();
             $table->mediumText('value');
             $table->integer('expiration');
         });
 
-        Schema::create('cache_locks', function (Blueprint $table) {
+        Schema::create(env('DB_CACHE_LOCK_TABLE'), function (Blueprint $table) {
             $table->string('key')->primary();
             $table->string('owner');
             $table->integer('expiration');
@@ -29,7 +29,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cache');
-        Schema::dropIfExists('cache_locks');
+        Schema::dropIfExists(env('DB_CACHE_TABLE'));
+        Schema::dropIfExists(env('DB_CACHE_LOCK_TABLE'));
     }
 };
