da30cbd1e10f501280da3c99987224cef1004cad
sonypiay <pyscho30@gmail.com>
2025-01-21 17:46:31 +0700

    create set score seeder

---
+++ database/seeders/SetScoreSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Users;
+use App\Models\HistoryScore;
+use App\Models\ScoreLeaderboard;
+
+class SetScoreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::transaction(function() {
+            HistoryScore::whereRaw(1)->delete();
+            ScoreLeaderboard::whereRaw(1)->delete();
+
+            Users::all()
+            ->lazy(100)
+            ->each(function($users) {
+                $score = mt_rand(1000,9999);
+
+                HistoryScore::create([
+                    'user_id' => $users->id,
+                    'level' => 1,
+                    'score' => $score,
+                ]);
+                
+                ScoreLeaderboard::create([
+                    'user_id' => $users->id,
+                    'level' => 1,
+                    'score' => $score
+                ]);
+            });
+        });
+    }
+}
